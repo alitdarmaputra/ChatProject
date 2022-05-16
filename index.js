5e3605571e143766947727a6bd44590cb9ee07f3
alitdarmaputra <alitdarmaputra@gmail.com>
2022-05-16 23:58:40 +0800

    Finishing initial feature

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-// require('dotenv').config()
+require('dotenv').config()
 const ejs = require("ejs");
 const express = require("express");
-
 const app = new express();
+
+const http = require("http");
+const server = http.createServer(app)
+const {Server} = require("socket.io");
+const io = new Server(server);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log("Tokaku chat server listen at port", port);
-});
 
 // Route Handler
 app.get("/", (req, res) => {
     res.render("chat");
+});
+
+io.on("connection", socket => {
+    socket.on("message", response => {
+        socket.broadcast.emit("message", {
+            "message": response,
+            "user_id": socket.id
+        });
+    });
+});
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log("Tokaku chat server listen at port", port);
 });
